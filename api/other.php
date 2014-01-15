59126945a1aa876d33f0360e2ecc460acd7778de
Terrence Ryan <terry@terrenceryan.com>
2014-01-15 14:37:16 -0500

    Tweaking the services.

--- api/other.php
+++ api/other.php
@@ -2,6 +2,6 @@
     include_once $_SERVER['DOCUMENT_ROOT'] . '/poll/config/creds.php';
     include_once $_SERVER['DOCUMENT_ROOT'] . '/poll/api/lib.php';
     
-    $entries = getQuestionInfo($DB);
+    $entries = getOtherInfo($DB);
     outputToJSON($entries);
 ?>
--- api/product.php
+++ api/product.php
@@ -1,24 +1,7 @@
 <?php
     include_once $_SERVER['DOCUMENT_ROOT'] . '/poll/config/creds.php';
-    
+    include_once $_SERVER['DOCUMENT_ROOT'] . '/poll/api/lib.php';
     
     $entries = getProductInfo($DB);
-    $rows = array();
-    while($r = mysql_fetch_assoc($entries)) {
-        $rows[] = $r;
-    }
-    print json_encode($rows);
-    
-    
-    function getProductInfo($dbInfo) {
-        $dbConn = mysql_connect($dbInfo['host'], $dbInfo['username'], $dbInfo['password']) or die(mysql_error());
-        mysql_select_db($dbInfo['db'], $dbConn) or die(mysql_error());
-        $entries = mysql_query(" SELECT * FROM familiar ", $dbConn) or die(mysql_error()); 
-        return $entries;
-           
-        
-    }
-    
-
-
+    outputToJSON($entries);
 ?>
